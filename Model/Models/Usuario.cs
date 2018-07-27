abbdd8eeb652542264870b9c505b119c7f7a83f1
yuri300 <yurisc50@gmail.com>
2018-07-27 20:04:48 -0300

    Adição da classe Usuário

---
+++ Model/Models/Usuario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.Models
+{
+    public class Usuario
+    {
+        public int Id;
+        public string Nome;
+    }
+}
